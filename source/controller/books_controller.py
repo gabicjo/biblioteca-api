f1120f812dd199d0ae2e60a7c8604398431ed080
gabicjo <gabriebs@proton.me>
2026-04-15 20:35:40 -0300

    feat: criar uma verificação para dados invalidos, como anos invalidos, emitindo um erro personalizado e tratando dentro da rota

--- source/controller/books_controller.py
+++ source/controller/books_controller.py
@@ -1,4 +1,5 @@
 from models import books_model
+from utils.errors import DadosInvalidosError
 
 
 def __verificar_colunas_faltantes(dados: dict, requeridos: list) -> list:
@@ -12,21 +13,29 @@ def __verificar_colunas_faltantes(dados: dict, requeridos: list) -> list:
 
 
 def __corrigir_dados_vazios(dados: dict) -> dict:
-    if "autor" not in dados or dados['autor'] == "":
+    if "autor" not in dados or dados["autor"] == "":
         dados["autor"] = "Desconhecido"
 
-    if "language" not in dados or dados['language'] == "":
+    if "language" not in dados or dados["language"] == "":
         dados["language"] = "english"
-    
-    if "status" not in dados or dados['status'] == "":
+
+    if "status" not in dados or dados["status"] == "":
         dados["status"] = "disponivel"
 
     return dados
 
 
 def __corrigir_dados_invalidos(dados: dict) -> dict:
-    if dados['status'] not in ["disponivel", "emprestado"]:
-        dados['status'] = "disponivel"
+    if dados["status"] not in ["disponivel", "emprestado"]:
+        dados["status"] = "disponivel"
+
+    if len(str(dados["publish_date"])) > 4:
+        raise DadosInvalidosError("Dados invalidos")
+
+    if dados["page_number"] < 30:
+        raise DadosInvalidosError("Dados invalidos")
+
+    return dados
 
 
 def verificar_dados_recebidos(dados: dict) -> str:
--- source/routes/books_route.py
+++ source/routes/books_route.py
@@ -1,15 +1,20 @@
 from flask import Flask, Blueprint, jsonify, request
 from models import books_model
 from controller import books_controller
+from utils.errors import DadosInvalidosError
 
 books_bp = Blueprint("books", __name__)
 
 @books_bp.route("/books/add", methods=["POST"])
 def listar_livros():
     dados = request.get_json()
-    verify = books_controller.verificar_dados_recebidos(dados)
+    try:
+        verify = books_controller.verificar_dados_recebidos(dados)
 
-    if verify == "error":
-        return {"message": "dados insuficientes"}, 400
+        if verify == "error":
+            return {"message": "dados insuficientes"}, 400
 
-    return {"message": "dados adicionados com sucesso"}, 201
+        return {"message": "dados adicionados com sucesso"}, 201
+        
+    except DadosInvalidosError:
+        return {"message": "dados invalidos"}, 400
