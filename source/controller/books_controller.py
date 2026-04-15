33ccddf7f945c356bac38dd32b2b551bbd4798b6
gabicjo <gabriebs@proton.me>
2026-04-15 20:09:32 -0300

    feat: verificar se o autor, lingua ou status não existem ou estão vazios e atribuir um valor padrão a eles.

--- source/controller/books_controller.py
+++ source/controller/books_controller.py
@@ -1,6 +1,7 @@
 from models import books_model
 
-def __verificar_colunas_faltantes(dados: dict, requeridos: list):
+
+def __verificar_colunas_faltantes(dados: dict, requeridos: list) -> list:
     faltando = []
 
     for item in requeridos:
@@ -9,11 +10,27 @@ def __verificar_colunas_faltantes(dados: dict, requeridos: list):
 
     return faltando
 
-def verificar_dados_recebidos(dados):
-    faltando = __verificar_colunas_faltantes(dados, ["name", "autor", "publish_date", "page_number", "genre"])
+
+def __corrigir_dados_vazios(dados: dict) -> dict:
+    if "autor" not in dados or dados['autor'] == "":
+        dados["autor"] = "Desconhecido"
+
+    if "language" not in dados or dados['language'] == "":
+        dados["language"] = "english"
+    
+    if "status" not in dados or dados['status'] == "":
+        dados["status"] = "disponivel"
+
+    return dados
+
+
+def verificar_dados_recebidos(dados: dict) -> str:
+    faltando = __verificar_colunas_faltantes(dados, ["name", "publish_date", "page_number", "genre"])
 
     if faltando:
         return "error"
+
+    dados = __corrigir_dados_vazios(dados)
 
     books_model.adicionar_livro_novo(dados)
     return "ok"
