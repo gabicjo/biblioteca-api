003782ae40435d5437280c3e6ae079d7d942e0a4
gabicjo <gabriebs@proton.me>
2026-04-15 19:01:19 -0300

    feat: criar uma função que cria uma tabela que armazena  dados sobre livros

--- source/models/books_model.py
+++ source/models/books_model.py
@@ -2,3 +2,19 @@ import sqlite3
 
 conexão = sqlite3.connect("banco.db")
 cursor = conexão.cursor()
+
+def criar_tabela_books():
+    cursor.execute("""CREATE TABLE IF NOT EXISTS livros (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    autor TEXT NOT NULL,
+                    publish_date INTEGER NOT NULL,
+                    page_number INTEGER NOT NULL,
+                    language TEXT NOT NULL,
+                    genre TEXT NOT NULL,
+                    status TEXT NOT NULL
+                    )""")
+
+criar_tabela_books()
+
+conexão.commit()
