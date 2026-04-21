e67fdbc0e4f8428c7bf0278bdfa304b22978f5c7
gabicjo <gabriebs@proton.me>
2026-04-21 07:47:13 -0300

    feat: criar uma nova coluna chamada users com id, name e password

---
+++ source/models/users_model.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+def criar_tabela_users():
+    conexão = sqlite3.connect("banco.db")
+    cursor = conexão.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL UNIQUE,
+                    password TEXT NOT NULL
+                    )""")
+    
+    conexão.commit()
+    conexão.close()
+
+
+criar_tabela_users()
